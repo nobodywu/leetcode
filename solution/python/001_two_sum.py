baef8c6a547bced5b902382256bbf2bf864c5891
whale wu <mengze_wu@foxmail.com>
2021-11-05 14:04:58 +0800

    add 001, 002 solutions of python

---
+++ solution/python/001_two_sum.py
@@ -0,0 +1,48 @@
+class Solution:
+    def twoSum1(self, nums: list, target: int) -> list:
+
+        for i, v1 in enumerate(nums):
+
+            for j, v2 in enumerate(nums):
+                if i == j:
+                    continue
+
+                if v1 + v2 == target:
+                    return [i, j]
+
+    def twoSum2(self, nums: list, target: int) -> list:
+
+        nums_length = len(nums)
+
+        for i in range(nums_length):
+
+            for j in range(i + 1, nums_length):
+
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+
+    def twoSum3(self, nums: list, target: int) -> list:
+        '''
+        使用字典，**求解速度最快**。
+        首先遍历一次，构造字典，列表中元素为字典key，列表的index为字典的value
+        再一次遍历，根据目标值和当前列表元素值只差，获取对应字典的value（即为index）
+        如果dict.get()的返回值不为Node且返回值不等于当前遍历的index，则输出结果
+        '''
+
+        a = {}
+        for i, v in enumerate(nums):
+            a[v] = i
+
+        for i, v in enumerate(nums):
+            diff_index = a.get(target-v)
+
+            if diff_index is not None and diff_index != i:
+                return [i, diff_index]
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.twoSum1([6,7,4], 11))
+    print(s.twoSum2([3,9,1], 4))
+    print(s.twoSum3([2,7,10], 9))
---
+++ solution/python/002_add_two_numbers.py
@@ -0,0 +1,75 @@
+import copy
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # 给要返回的链表建个表头，这样通过 answer.next 即可拿到要返回的链表
+        answer = ListNode(0)
+
+        # print(id(answer))
+        # body = copy.copy(answer)
+        # print(id(body))
+        # body = copy.deepcopy(answer)
+        # print(id(body))
+
+        body = answer # 这里的赋值为引用，浅拷贝或深拷贝都会创建新的地址
+        # print(id(body))
+
+        # 进位默认为 0 
+        carry = 0
+        # 将 l1 和 l2 循环二合一
+        while l1 != None or l2 != None:
+            # 利用 Python 中的三元表达式简化取值过程
+            if l1 != None:
+                x = l1.val
+                l1 = l1.next
+            else:
+                x = 0
+
+            if l2 != None:
+                y = l2.val
+                l2 = l2.next
+            else:
+                y = 0
+
+            # 结果要加上进位
+            sum_val = x + y + carry
+            # 更新下一位的进位
+            carry = sum_val // 10
+            # 将结果定义为 ListNode 并赋给我们的下一节点
+            body.next = ListNode(sum_val % 10)
+            # 更新 body 为下一节点以完成连接
+            body = body.next
+
+        # 考虑个最后一位的情况
+        if carry > 0:
+            body.next = ListNode(carry)
+          
+        return answer.next
+
+if __name__ == "__main__":
+    # 2 -> 4 -> 3
+    nodelist1 = ListNode(2)
+    nodelist1.next = ListNode(4)
+    nodelist1.next.next = ListNode(3)
+
+    # 5 -> 6 -> 4
+    nodelist2 = ListNode(5)
+    nodelist2.next = ListNode(6)
+    nodelist2.next.next = ListNode(4)
+
+    s = Solution()
+    a = s.addTwoNumbers(nodelist1, nodelist2)
+    
+    if None != a:
+        print(a.val)
+        while None != a.next:
+            a = a.next
+            print(a.val)
+
