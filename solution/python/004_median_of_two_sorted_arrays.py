75a190853b9be0260f7914488c2aeb765f489d64
whale wu <mengze_wu@foxmail.com>
2021-11-08 11:26:02 +0800

    modify comment for 004

--- solution/python/004_median_of_two_sorted_arrays.py
+++ solution/python/004_median_of_two_sorted_arrays.py
@@ -1,9 +1,10 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: list, nums2: list) -> float:
 
-        # 注意list的extend和sort方法没有返回值
+        # list的extend和sort方法没有返回值
         nums1.extend(nums2)
-        nums1.sort()
+        # list.sort(), sorted(list)
+        nums1.sort() # python的排序算法为timsort，对任意长度的数组来说是最优的
 
         if len(nums1)%2 == 0:
             i = len(nums1)//2
