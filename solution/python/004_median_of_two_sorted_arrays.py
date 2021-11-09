2d4b44141fb7e6813c0873c31f2bb8de49d0047c
whale wu <mengze_wu@foxmail.com>
2021-11-09 17:48:32 +0800

    add solution 2 for 004

--- solution/python/004_median_of_two_sorted_arrays.py
+++ solution/python/004_median_of_two_sorted_arrays.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: list, nums2: list) -> float:
+    def findMedianSortedArrays1(self, nums1: list, nums2: list) -> float:
 
         # list的extend和sort方法没有返回值
         nums1.extend(nums2)
@@ -14,6 +14,52 @@ class Solution:
             i = len(nums1)//2
             return nums1[i]
 
+    def findMedianSortedArrays2(self, nums1: list, nums2: list) -> float:
+
+        def getKthEle(k):
+            index1, index2 = 0, 0
+
+            while True:
+                # 如果上一循环出现下标越界的，则index = new_index1 + 1恰好为数组长度，此时上一循环计算出的k值无意义 
+                # 如果为空数组，返回另一个数组的index(0) + k - 1下标值
+                if m == index1:
+                    return nums2[index2 + k - 1]
+                if n == index2:
+                    return nums1[index1 + k - 1]
+
+                if 1 == k:
+                    return min(nums1[index1], nums2[index2])
+                
+                # 正常情况
+                # 计算当前数组下标
+                new_index1 = min(index1 + (k//2 - 1), m-1) # 如果下标越界，则指向数组最后一个
+                new_index2 = min(index2 + (k//2 - 1), n-1)
+
+                ele1 = nums1[new_index1] # 取出数组中的元素
+                ele2 = nums2[new_index2]
+
+                if ele1 < ele2:
+                    k = k - (new_index1 -index1 + 1) # 更新k值
+                    index1 = new_index1 + 1 # 数组下标指向下一个位置
+                else:
+                    k = k - (new_index2 -index2 + 1)
+                    index2 = new_index2 + 1
+
+
+        m = len(nums1)
+        n = len(nums2)
+
+        k = (m + n)//2
+
+        if (m + n) % 2 == 0:
+            return (getKthEle(k) + getKthEle(k+1))/2
+
+        else:
+            return getKthEle(k+1)
+
+
 if __name__ == "__main__":
     s = Solution()
-    print(s.findMedianSortedArrays([1,8,3],[4,5,6]))
+    # nums1, muns2每个数组内已经排序
+    print(s.findMedianSortedArrays2([1,3,8],[4,5,6])) # 中位数为4.5
+    print(s.findMedianSortedArrays([],[1])) # 中位数为4.5
