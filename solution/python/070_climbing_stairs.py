bfea2ca33282cbad189dbc85d4f3c0594f718c9a
whale wu <mengze_wu@foxmail.com>
2021-12-06 15:26:57 +0800

    add 070

---
+++ solution/python/070_climbing_stairs.py
@@ -0,0 +1,37 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''
+        动态规划方法求解
+        '''
+        if 1 == n:
+            return 1
+
+        if 2 == n:
+            return 2
+
+        ans = [1, 2] + [0] * (n - 2)
+
+        # 爬到第n节台阶有f(n) = f(n - 1) + f(n - 2)种方法
+        for i in range(2, n):
+            ans[i] = ans[i - 1] + ans[i - 2]
+
+        return ans[-1]
+
+    def climbStairs2(self, n: int) -> int:
+        '''
+        动态规划方法求解，不使用数组
+        '''
+        pass
+
+    def climbStairs3(self, n: int) -> int:
+        '''
+        数学方法求解
+        '''
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.climbStairs(2))
+    print(s.climbStairs(3))
+    print(s.climbStairs(4))
