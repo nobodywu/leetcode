101aaf894b4040a617afe46cb2b9fe01cb34e2e9
whale wu <mengze_wu@foxmail.com>
2021-12-22 12:19:34 +0800

    add 105

--- solution/python/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ solution/python/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -17,30 +17,29 @@ class Solution:
         后续遍历（左右根）: 先访问左子树，再访问右子树，再访问根结点。
         层序遍历
         '''
-        def myBuildTree(preorder_left: int, preorder_right: int, inorder_left: int, inorder_right: int):
-            def findChild(inorder: list):
-                ele = preorder.pop(0)
-                node = TreeNode(ele)
-                ele_idx = inorder.index(ele)
-                left_sub_tree = inorder[:ele_idx]
-                right_sub_tree = inorder[ele_idx + 1:]
-                
-                if left_sub_tree:
-                    node.left = findChild(left_sub_tree)
-                else:
-                    node.left = None
+        def findChild(inorder: list):
+            ele = preorder.pop(0) # 前序遍历的第一个元素为根节点
+            node = TreeNode(ele)
+            ele_idx = inorder.index(ele)
+            left_sub_tree = inorder[:ele_idx] # 中序遍历中的左子树
+            right_sub_tree = inorder[ele_idx + 1:] # 中序遍历中的右子树
+            
+            if left_sub_tree:
+                node.left = findChild(left_sub_tree) # 在中序遍历的左子树中查找当前节点的左子节点。对中序遍历不断分割，直到最后一个节点
+            else:
+                node.left = None
 
-                if right_sub_tree:
-                    node.right = findChild(right_sub_tree)
-                else:
-                    node.right = None
+            if right_sub_tree:
+                node.right = findChild(right_sub_tree) # 在中序遍历的右子树中查找当前节点的右子节点。对中序遍历不断分割，直到最后一个节点
+            else:
+                node.right = None
 
-                return node
+            return node
 
-            if not preorder or not inorder:
-                return None
+        if not preorder or not inorder:
+            return None
 
-            return findChild(inorder)
+        return findChild(inorder)
 
 if __name__ == "__main__":
     '''
